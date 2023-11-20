be52213140ecc17ff147a14ce41513f1b792ec19
Your Name <you@example.com>
2023-11-20 19:31:06 +0300

    first commit

---
+++ src/New/Main.java
@@ -0,0 +1,16 @@
+package New;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("Murzik", 1, 12, "Red");
+        animal.saySay();
+        animal.sayDrink();
+
+        System.out.println(animal);
+    }
+}
+
+
+
